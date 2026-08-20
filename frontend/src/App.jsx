89a94966e7a898c85bcb34a4624f63c675736986
AnanthChavan <ananthchavan2006@gmail.com>
2026-08-20 17:40:19 +0530

    feat(frontend): implement React Router wiring

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+
+import MainLayout from './layouts/MainLayout';
+
+import Home from './pages/Home';
+import SearchResults from './pages/SearchResults';
+import CrawlerAdmin from './pages/CrawlerAdmin';
+import AnalyticsAdmin from './pages/AnalyticsAdmin';
 
 function App() {
   return (
-    <h1>Working</h1>
-  )
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="search" element={<SearchResults />} />
+        <Route path="admin/crawler" element={<CrawlerAdmin />} />
+        <Route path="admin/analytics" element={<AnalyticsAdmin />} />
+
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
